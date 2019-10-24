1135ea8c96cba1a343fc6a46269543af32b43c42
rizwanishaq <rizwanishaq@gmail.com>
2019-10-24 16:52:30 +0200

    another hour complete with mongodb(sort, filtering) etc

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const {
-    getBootcamps,
-    getBootcamp,
-    createBootcamp,
-    updateBootcamp,
-    deleteBootcamp
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius
 } = require("../controllers/bootcamps");
 
 const router = express.Router();
 
-router
-    .route("/")
-    .get(getBootcamps)
-    .post(createBootcamp);
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
 router
-    .route("/:id")
-    .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp);
+  .route("/")
+  .get(getBootcamps)
+  .post(createBootcamp);
+
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
